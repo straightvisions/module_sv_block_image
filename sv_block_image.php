20978900f3938cc61a60d1cce7c3bb36ea5a2f0c
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-22 22:05:40 +0200

    refactoring: - remove placeholder files - migrate extra styles registration from JS to PHP - remove legacy extra styles

--- sv_block_image.php
+++ sv_block_image.php
@@ -12,18 +12,9 @@
 				->set_section_order(5000)
 				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 8.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5zm9 .5l-2.519 4-2.481-1.96-4 5.96h14l-5-8zm8-4v14h-20v-14h20zm2-2h-24v18h24v-18z"/></svg>')
 				->set_block_handle('wp-block-image')
+				->set_block_name('core/image')
 				->get_root()
 				->add_section( $this );
-		}
-		protected function register_scripts(): sv_block_image {
-			parent::register_scripts();
-
-			// Register Styles
-			$this->get_script( 'style_no_margin' )
-				->set_is_gutenberg()
-				->set_path( 'lib/css/common/style_no_margin.css' );
-
-			return $this;
 		}
 		protected function load_settings(): sv_block_image {
 			$this->get_setting( 'margin' )
